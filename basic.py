ea90c14c4f9489237626272015467f6b59a8e982
ruqaiya85 <56188115+ruqaiya85@users.noreply.github.com>
2019-10-05 13:43:43 +0530

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,40 @@
+li=["apple","banana","mango"]
+print(li)
+#print(tup)
+#tup.append(4)
+#print(tup)
+#dic={"name" : 'rukaiya',"college" : 'aiktc',"age" :'18'}
+#print(dic["college"])
+#list={12,13,14}
+#print(list[12])
+#n=10
+#if n==1:
+ #    print("hello")
+#elif n==9:
+ #   print("bad") 
+#elif n==2:
+ #   print("good")
+#else:
+ #   print("bye")
+#for i  in range(0,100,5):
+ #   print(i)
+ #for i in range(100,5,-6):
+  #  print(i)
+#list=['a','b','c']
+ #      if(i%5):
+  #          print("")
+#print("WELCOME TO THE WORLD OF PYTHON")
+#def add(a,b):
+ #   c=a+b
+  #  return c
+
+#addition=add(44,66)
+#print(addition)
+'''b=['a','bb','ccc']
+for i in b:
+  print(i)'''
+
+
+
+
+    
